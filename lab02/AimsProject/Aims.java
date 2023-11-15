ddfc547459027a72e4c9dcbab035f46c25e0d24e
Vietanh-Vu <vuvietanh993@gmail.com>
2023-11-15 18:06:24 +0700

    add search method

--- lab02/AimsProject/Aims.java
+++ lab02/AimsProject/Aims.java
@@ -7,37 +7,44 @@ public class Aims {
 
         //Create new dvd objects and add them to the cart
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        if (anOrder.addDigitalVideoDisc(dvd1)) {
-            System.out.println("Add successful");
-        }
-
-
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        if (anOrder.addDigitalVideoDisc(dvd2)) {
-            System.out.println("Add successful");
-        }
-
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        if (anOrder.addDigitalVideoDisc(dvd3)) {
+        DigitalVideoDisc[] arrayDvd = {dvd1, dvd2, dvd3};
+        if (anOrder.addDigitalVideoDisc(arrayDvd)) {
             System.out.println("Add successful");
             System.out.println();
         }
 
-        anOrder.showCart();
+        anOrder.searchDVDById(2);
 
-        //print total cost of the items in the cart
-        System.out.println("Total Cost is: " + String.format("%.2f", anOrder.calculateTotalCost()));
-        System.out.println();
 
-        // Remove the dvd1
-        if (anOrder.removeDigitalVideoDisc(dvd1)) {
-            System.out.println("Remove successful");
-            System.out.println();
-        }
+//        if (anOrder.addDigitalVideoDisc(dvd1)) {
+//            System.out.println("Add successful");
+//        }
 
-        anOrder.showCart();
 
-        //print total cost of the items in the cart after remove
-        System.out.println("Total Cost after remove is: " + String.format("%.2f", anOrder.calculateTotalCost()));
+//        if (anOrder.addDigitalVideoDisc(dvd2)) {
+//            System.out.println("Add successful");
+//        }
+
+//        if (anOrder.addDigitalVideoDisc(dvd3)) {
+//            System.out.println("Add successful");
+//            System.out.println();
+//        }
+
+//        //print total cost of the items in the cart
+//        System.out.println("Total Cost is: " + String.format("%.2f", anOrder.calculateTotalCost()));
+//        System.out.println();
+//
+//        // Remove the dvd1
+//        if (anOrder.removeDigitalVideoDisc(dvd1)) {
+//            System.out.println("Remove successful");
+//            System.out.println();
+//        }
+//
+//        anOrder.showCart();
+//
+//        //print total cost of the items in the cart after remove
+//        System.out.println("Total Cost after remove is: " + String.format("%.2f", anOrder.calculateTotalCost()));
     }
 }
